4e272b9cb7e0d12aac88f0e8c3263e602aed6d1e
hollystyles <styles.holly@googlemail.co.uk>
2013-05-26 12:23:25 +0100

    Added CsvReader and Tests

---
+++ Hollyathome.IO/CsvReader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Hollyathome.IO
+{
+    /// <summary>
+    /// Used to read a comma delimited text file or stream.
+    /// </summary>
+    public class CsvReader : StreamReader
+    {
+        #region Constructors
+
+        public CsvReader(Stream stream)
+            : base(stream)
+        {
+        }
+
+        public CsvReader(Stream stream, Encoding encoding)
+            : base(stream, encoding)
+        {
+        }
+
+        public CsvReader(string filePath)
+            : base(filePath)
+        {
+        }
+
+        public CsvReader(string path, Encoding encoding)
+            : base(path, encoding)
+        {
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Reads the current line in the stream and returns an 
+        /// array of the delimited values
+        /// </summary>
+        /// <returns></returns>
+        public string[] ReadRow()
+        {
+            List<string> row = new List<string>();
+            bool inQuotedString = false;
+            bool escapeNext = false;
+            string val = string.Empty;
+            string line = base.ReadLine();
+
+            foreach (char c in line)
+            {
+                if (c == '\\' && !escapeNext)
+                {
+                    escapeNext = true;
+                }
+                else if (c == '"' && !escapeNext)
+                {
+                    inQuotedString = !inQuotedString;
+                }
+                else if (c == ',' && !inQuotedString)
+                {
+                    row.Add(val);
+                    val = string.Empty;
+                }
+                else
+                {
+                    val += c;
+                    escapeNext = false;
+                }
+            }
+            
+            row.Add(val);
+            
+            return row.ToArray();
+        }
+
+        #endregion
+
+    }
+}
---
+++ UnitTests/CsvReaderTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hollyathome.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CsvReaderTests
+    {
+        [TestMethod]
+        public void ReadHeaders()
+        {
+            string[] expectedValues = new string[]{"ID","Value1","Value2","Value3"};
+            
+            ReadAndAssert("test1.csv", 0, expectedValues);            
+        }
+
+        [TestMethod]
+        public void ReadSecondRowWithBackslash()
+        {
+            string[] expectedValues = new string[] { "1", "String0 with a \\ backslash", "1", "1.0" };
+
+            ReadAndAssert("test1.csv", 1, expectedValues);
+        }
+
+        [TestMethod]
+        public void ReadThirdRowWithEmbeddedComa()
+        {
+            string[] expectedValues = new string[] { "2", "String1, with comma", "2", "2.0" };
+
+            ReadAndAssert("test1.csv", 2, expectedValues);
+        }
+
+        [TestMethod]
+        public void ReadFourthRowWithEscapedQuotes()
+        {
+            string[] expectedValues = new string[] { "3", "String2 \"quoted\" with escape sequence", "3", "3.0" };
+
+            ReadAndAssert("test1.csv", 3, expectedValues);
+        }
+
+        private void ReadAndAssert(string path, int lineIndex, string[] expectedValues)
+        {
+            using (var t = new CsvReader(path, System.Text.Encoding.Default))
+            {
+                while (lineIndex > 0)
+                {
+                    t.ReadLine();
+                    lineIndex--;
+                }
+
+                string[] actualValues = t.ReadRow();
+
+                for (int i = 0; i < 4; i++)
+                {
+                    Assert.AreEqual(expectedValues[i], actualValues[i]);
+                }
+            }
+        }
+    }
+}
